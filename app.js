728756075f662914de59d02f25da9a6c3f9d0f0a
Guilherme <guilhermepm164@gmail.br>
2021-03-10 19:43:54 -0300

    conected to db postgres

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ app.get('/', (req, res) => {
 app.listen(port, (err) => {
     return console.error(err)
 })
+
+/**
+ * para entrar no db só rodar{
+ * docker run --name postgres -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=123456 -e POSTGRES_DB=db_teste -d postgres
+ * } o mesmo, abrir o shell{
+ * docker exec -it af81fe9489f93212b146db34c7f88b72acfda9d42537cb819eabd0ffda3dd8b2 bash
+ * } e rodar {
+ * psql db_teste root
+ * }
+ */
