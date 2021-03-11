4618132f8fdc747f88e458e57c38d332e410e7f6
Guilherme <guilhermepm164@gmail.br>
2021-03-11 17:36:17 -0300

    validator/create ok

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -1,9 +1,26 @@
 import { Request, Response } from 'express';
 import User from '../models/user'
+import yup from 'yup';
 
 class UserController {
     async create(request: Request, response: Response) {
-        //validation here
+        const schema = yup.object().shape({
+            firstName: yup.string().required("First Name is Required"),
+            lastName: yup.string().required("Last Name is Required"),
+            email: yup.string().email().required("Email is Required"),
+            password: yup.string().required("Password is Required")
+                .matches(RegExp("(.*[a-z].*)"), "Lowercase")
+                .matches(RegExp("(.*[A-Z].*)"), "Uppercase")
+                .matches(RegExp("(.*\\d.*)"), "Number")
+                .matches(RegExp("/[a-zA-Z0-9]\z/"), "Without Special Characters")
+            // '[!@#$%^&*(),.?":{}|<>]'), "Special" <<habilite special characters
+        })
+        try {
+            await schema.isValid(request.body, { abortEarly: false })
+        } catch (error) {
+            const err = error.errors;
+            return response.status(400).json({ err })
+        }
 
         const user_db = await User.findByPk(request.params.email)
         if (user_db) {
@@ -15,6 +32,8 @@ class UserController {
         }
         return response.status(201).json();
     }
+
+
     async update(request: Request, response: Response) {
         //validation here
 
@@ -27,10 +46,9 @@ class UserController {
         }
         return response.status(201).json();
     }
+
+
     async find(request: Request, response: Response) {
-        //validation here
-
-
         const db_users = await User.findAll();
         if (!db_users) {
             return response.status(400).json({ error: "You Need Create Users" })
@@ -38,6 +56,8 @@ class UserController {
             return response.status(201).json(db_users);
         }
     }
+
+
     async findOne(request: Request, response: Response) {
         //validation here
 
@@ -49,6 +69,8 @@ class UserController {
             return response.status(201).json(user_db);
         }
     }
+
+
     async delete(request: Request, response: Response) {
         //validation here
 
@@ -61,10 +83,9 @@ class UserController {
         }
         return response.status(201).json();
     }
+
+
     async deleteAll(request: Request, response: Response) {
-        //validation here
-
-
         const db_users = await User.findAll()
         if (!db_users) {
             return response.status(400).json({ error: "You Need Create Users" })
@@ -75,7 +96,6 @@ class UserController {
         }
         return response.status(201).json(db_users)
     }
-
-
 }
+
 export { UserController }
